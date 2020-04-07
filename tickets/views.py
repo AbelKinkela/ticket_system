e9f42bffa00abb9e7fbf2d8efa5bb2b7d6744e96
AbelKinkela <akinkela16@alustudent.com>
2020-04-07 12:44:44 +0100

    commit 2: two pages now(index, about). Also added base.html

--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
